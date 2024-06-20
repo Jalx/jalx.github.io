5f378fb4102c67aa8f5c491004fcb79d173f6e52
Alexis Guzmán <alexis.guzman27@outlook.com>
2024-06-19 23:15:22 -0600

    Using astro as a portfolio CMS
    Add config files
    Add boilerplate: layouts and tailwind
    Add CSS files

---
+++ portfolio/astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind({
+      nesting: true,
+      applyBaseStyles: false
+    }),
+    react()],
+  // i18n: {
+  //   defaultLocale: "en",
+  //   locales: ["es", "en"]
+  // }
+});
